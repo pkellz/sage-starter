e29c05d4e8cd92efa0a6b0565972b48f16351ebf
pkellz <patrickscott2013@gmail.com>
2022-07-16 21:30:18 -0400

    - Update variable name

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const sageComponent = <h1 title="web dev made simple">DevSage</h1>;
+const element = <h1 title="web dev made simple">DevSage</h1>;
 const root = document.getElementById("root")
-ReactDOM.render(sageComponent, root);
+ReactDOM.render(element, root);
